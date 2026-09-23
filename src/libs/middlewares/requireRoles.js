429b53eaee7bb5d3cccbbd73c1b74dc87c301ae4
fabian <fabi.lillo@duocuc.cl>
2026-09-23 17:06:41 -0300

    Add debug logging to requireRoles middleware for improved traceability

--- src/libs/middlewares/requireRoles.js
+++ src/libs/middlewares/requireRoles.js
@@ -1,7 +1,14 @@
 const { getUserFromEvent } = require('./auth');
+const logger = require('../utils/logger'); // ajusta el path si es necesario
 
 const requireRoles = (...allowedRoles) => (handler) => async (event, context) => {
   const user = getUserFromEvent(event);
+
+  logger.info('DEBUG requireRoles', {
+    rawClaims: event?.requestContext?.authorizer?.jwt?.claims,
+    userRoles: user?.roles,
+    allowedRoles,
+  });
 
   if (!user) {
     const err = new Error('No se pudo identificar al usuario autenticado');
